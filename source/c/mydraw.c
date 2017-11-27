4a5fbb9d9bbf6f9cdb4d04fc362277009416866a
Dongdongshe <shrek.s@hotmail.com>
2017-11-27 15:22:20 -0500

    add render() and put_wall() put_bed()

--- source/c/mydraw.c
+++ source/c/mydraw.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
+
+/* OpenGL coordinate range X = [-2,2], Y = [-2, 2], coordinate obtained from FPL is X=[0,10], Y = [0,10] , so we would do a coordinate calibration */
+
+//global float array to store every object's data to be drawn
+static float data[10][6] =  {{0,0,0,0,0,0}};
+static int index = 0;
 //Called when a key is pressed
 void handleKeypress(unsigned char key, int x, int y) {
 	switch (key) {
@@ -34,7 +40,7 @@ void draw_rec() {
 	glBegin(GL_QUADS); //Begin quadrilateral coordinates
 glColor3f(0, 0, 0);	//Trapezoid
 	glVertex3f(-0.7f, -1.5f, -5.0f);
-	glVertex3f(0.7f, -1.5f, -5.0f);
+	glVertex3f(0.7F, -1.5f, -5.0f);
 	glVertex3f(0.7f, -0.5f, -5.0f);
 	glVertex3f(-0.7f, -0.5f, -5.0f);
 	glEnd(); //End quadrilateral coordinates
@@ -48,7 +54,12 @@ void draw_line() {
 	glMatrixMode(GL_MODELVIEW); //Switch to the drawing perspective
 	glLoadIdentity(); //Reset the drawing perspective
 	glBegin(GL_LINES); //Begin quadrilateral coordinates
-glColor3f(0, 0, 0);	//Trapezoid
+    glColor3f(0, 0, 0);	//Trapezoid
+	glVertex3f(1.0f, -2.0f, -5.0f);
+	glVertex3f(1.0f, 1.5f, -5.0f);
+	glEnd(); //End quadrilateral coordinates
+	glBegin(GL_LINES); //Begin quadrilateral coordinates
+    glColor3f(0, 0, 0);	//Trapezoid
 	glVertex3f(-1.0f, -1.5f, -5.0f);
 	glVertex3f(-1.0f, 1.5f, -5.0f);
 	glEnd(); //End quadrilateral coordinates
@@ -79,7 +90,7 @@ void drawRec(int a)
 	//Initialize GLUT
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(400, 400); //Set the window size
+	glutInitWindowSize(1000, 1000); //Set the window size
 	//Create the window
 	glutCreateWindow("Basic Shapes - programming-technique.blogspot.com");
 	initRendering(); //Initialize rendering
@@ -90,6 +101,79 @@ void drawRec(int a)
 	glutMainLoop(); //Start the main loop
 }
 
+void put_bed(float x, float y, int degree, float shiftX, float shiftY){
+    data[index][0] = 2.0;
+    data[index][1] = x*0.4 - 2.0 + shiftX * 0.4;
+    data[index][2] = y*0.4 - 2.0 + shiftY * 0.4;
+    data[index][3] = (float) degree;
+    data[index][4] = shiftX*0.4 -2;
+    data[index][5] = shiftY*0.4 - 2;
+    printf("x:%f, y:%f, degree:%f, shiftX:%f, shigtY:%f\n, sss:%f\n", data[index][0], data[index][1], data[index][2], data[index][3], data[index][4], data[index][5]);
+    index = index + 1;
+}
+
+void put_wall(float x, float y, int degree, float shiftX, float shiftY){
+    data[index][0] = 1.0;
+    data[index][1] = x*0.4 - 2.0 + shiftX * 0.4;
+    data[index][2] = y*0.4 - 2.0 + shiftY * 0.4;
+    data[index][3] = (float) degree;
+    data[index][4] = shiftX*0.4 -2;
+    data[index][5] = shiftY*0.4 - 2;
+    printf("x:%f, y:%f, degree:%f, shiftX:%f, shigtY:%f\n, sss:%f\n", data[index][0], data[index][1], data[index][2], data[index][3], data[index][4], data[index][5]);
+    index = index + 1;
+}
+
+void draw()
+{
+	//Clear information from last draw
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW); //Switch to the drawing perspective
+	glLoadIdentity(); //Reset the drawing perspective
+    
+    int len = index;
+    printf("len : %d\n",len);
+    for(int i = 0; i < len; i++)
+    {
+        if((int) data[i][0] == 1)
+        {                
+            glBegin(GL_LINES); //Begin quadrilateral coordinates
+            glColor3f(0, 0, 0);	//Trapezoid
+	        glVertex3f(data[i][1], data[i][2], -5.0f);
+	        glVertex3f(data[i][4], data[i][5], -5.0f);
+	        glEnd(); //End quadrilateral coordinates
+        }
+        if((int) data[i][0] == 2)
+        {                
+	        glBegin(GL_QUADS); //Begin quadrilateral coordinates
+            glColor3f(0, 0, 0);	//Trapezoid
+	        glVertex3f(data[i][4], data[i][5], -5.0f);
+	        glVertex3f(data[i][1], data[i][5], -5.0f);
+	        glVertex3f(data[i][1], data[i][2], -5.0f);
+	        glVertex3f(data[i][4], data[i][2], -5.0f);
+	        glEnd(); //End quadrilateral coordinates
+        }
+        
+    }
+	glutSwapBuffers(); //Send the 3D scene to the screen
+}
+
+void render(void)
+{
+    int argc = 1;
+    char *argv[1]={""};
+	//Initialize GLUT
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(1000, 1000); //Set the window size
+	//Create the window
+	glutCreateWindow("Basic Shapes - programming-technique.blogspot.com");
+	initRendering(); //Initialize rendering
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);//Set handler functions for drawing, keypresses, and window resizes
+	glutDisplayFunc(draw);
+	glutKeyboardFunc(handleKeypress);
+	glutReshapeFunc(handleResize);
+	glutMainLoop(); //Start the main loop 
+}
 /*
 int main(int argc, char** argv) {
 	drawLine(1);
